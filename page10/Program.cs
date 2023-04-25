1bf539443d457752455127c0648e9186987fb376
Danila Oreshkin <korobkamy50@yandex.ru>
2023-04-25 22:11:59 +0300

    P10

---
+++ page10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+
+void SecondNumber(int number)
+{
+    if (number > 100 && number < 1000)
+    {
+        int a = number / 10;
+        int b = a % 10;
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine("Введите трехзначтное число");
+    }
+}
+
+SecondNumber(count);
