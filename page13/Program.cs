8271b0740e3e58f6688476bdd5a177f51d7e40a3
Danila Oreshkin <korobkamy50@yandex.ru>
2023-04-25 22:32:59 +0300

    P13

---
+++ page13/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+
+void UselessProgram (int number)
+{
+    int a = number%10;
+    if (number > 99 && number < 1000)
+    {
+        Console.WriteLine(a);
+    }
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine("Введите трехзначтное число");
+    }
+}
+
+
+UselessProgram (count);
